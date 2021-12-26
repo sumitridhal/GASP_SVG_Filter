ba1e65413bdd7b14696968c256496286ca6fa4b7
sumitridhal <sumitridhal@gmail.com>
2021-12-26 23:12:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ktid2a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ktid2a)
---
+++ index.js
@@ -0,0 +1,14 @@
+import { Cursor } from './src/js/cursor';
+import { Item } from './src/js/item';
+
+// initialize custom cursor
+const cursor = new Cursor(document.querySelector('.cursor'));
+
+// items/images elems
+[...document.querySelectorAll('.item')].forEach((item) => new Item(item));
+
+// mouse effects on all links
+[...document.querySelectorAll('a, .distort__img')].forEach((link) => {
+  link.addEventListener('mouseenter', () => cursor.enter());
+  link.addEventListener('mouseleave', () => cursor.leave());
+});
